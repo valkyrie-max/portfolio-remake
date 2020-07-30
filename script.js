cfdf9b07c63c6f922cf9aa811467513c97aa57f8
valkyrie-max <alisakitkina2016@gmail.com>
2020-07-30 10:11:11 -0500

    switched header animations

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $( document ).ready(function() {
-    console.log( `hello world` );
+    // smooth scroll 
+    $('a[href^="#"]').click(function () {
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 1500);
+        return false;
+    });
+
+    // animate on scroll
     AOS.init();
 });
